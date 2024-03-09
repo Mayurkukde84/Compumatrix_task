5601b47d195ec1cf28f9eb1f0660237d74e40f5a
mayur <mayurkukde84@gmail.com>
2024-03-09 07:36:18 +0530

    initial app

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { default: mongoose } = require("mongoose");
+const connectDB = require("./config/connectDB");
+
+PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
+app.use('*',(req,res)=>{
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname,'views','404.html'))
+    }
+})
+connectDB()
+mongoose.connection.once('open',()=>{
+    console.log('mongodn is connected')
+    app.listen(PORT, () => {
+        console.log(`server is running on ${PORT}`);
+      });
+})
+
+mongoose.connection.on('err',()=>{
+    console.log(err)
+})
+
+
